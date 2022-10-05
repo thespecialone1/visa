add2e5bae2afb86bf44d8fe9edcf9900f65fadbd
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-10-05 19:47:26 +0500

    update

---
+++ partials/header.php
@@ -0,0 +1,193 @@
+<!-- /.topbar -->
+<div class="header-transparent">
+        <div class="topbar-transparent">
+            <!-- topbar transparent-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 d-none d-sm-none d-lg-block d-xl-block">
+                        <p class="welcome-text">Welcome to visapress a immigration company website template</p>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div class="header-block">
+                            <span class="header-link d-none d-xl-block d-md-block"><a href="#">Talk to Our Expert</a></span>
+                            <span class="header-link">+1 800 123 4567</span>
+                            <span class="header-link"> <button type="submit" class="" data-toggle="modal" data-target="#searchModal"> <i class="fa fa-search text-white"></i></button></span>
+                            <span class="header-link"><a href="#" class="btn btn-default btn-sm">Apply Now</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.topbar transparent-->
+        <!-- header classic -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12"><a href="index-3.html" class="logo"><img src="images/logo-3.png" alt="Visapress an Immigration and Visa Consulting Website Template"></a></div>
+                <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-xs-12">
+                    <div id="navigation-transparent" class="navigation-transparent">
+                        <!-- navigation -->
+                        <ul>
+                            <li><a href="#">Home</a>
+                                <ul>
+                                    <li><a href="index-2.html">Homepage v1</a></li>
+                                    <li><a href="index-4.html">Homepage v2</a></li>
+                                    <li><a href="index-3.html">Homepage v3</a></li>
+                                    <li><a href="index-5.html">Homepage v4</a></li>
+                                    <li><a href="index-6.html">Homepage v5</a></li>
+                                    <li><a href="index-7.html">Homepage v6</a></li>
+                                    <li><a href="#">Landing pages</a>
+                                        <ul>
+                                            <li><a href="landing-page-v1.html">Landing page v1</a></li>
+                                            <li><a href="landing-page-country-v1.html">Landing page country v1</a></li>
+                                            <li><a href="landing-page-ebook-v3.html">Landing page v3 - eBook</a></li>
+                                            <li><a href="landing-page-webinar.html">Landing page v4 - Webinars</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Coaching</a>
+                                <ul>
+                                    <li><a href="coaching-list-page.html">Coaching List</a></li>
+                                    <li><a href="coaching-sidebar.html">Coaching Sidebar</a></li>
+                                    <li><a href="coaching-full-page.html">Coaching Full Page</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Visas</a>
+                                <ul>
+                                    <li><a href="visa-immigration-list-page.html">Visa List Page</a></li>
+                                    <li><a href="visa-immigration-single-page.html">Visa Single Page</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Country</a>
+                                <ul>
+                                    <li><a href="country-list-page.html">Country List Page</a></li>
+                                    <li><a href="country-img-list-page.html">Country List Page v2 </a></li>
+                                    <li><a href="country-single-page.html">Country Single Page</a></li>
+                                    <li><a href="country-single-full-page.html">Country Full Page</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">About us </a>
+                                <ul>
+                                    <li><a href="#">About Us</a>
+                                        <ul>
+                                            <li><a href="about-us-page-v1.html">About us v1</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="founder-page.html">Founder page</a></li>
+                                    <li><a href="agent-list-page.html">Agent List</a></li>
+                                    <li><a href="agent-single-page.html">Agent Single</a></li>
+                                    <li><a href="partner-university-page.html">Partner University Page</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Features </a>
+                                <ul>
+                                    <li><a href="#">Testimonial</a>
+                                        <ul>
+                                            <li><a href="customer-review-page-v1.html">Customer Review Page v1</a></li>
+                                            <li><a href="customer-review-score-page-v2.html">Customer Review page v2</a></li>
+                                            <li><a href="video-testimonial-page.html">Video Testimonial</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Email List Builder</a>
+                                        <ul>
+                                            <li><a href="list-builder-embeded.html"> List Builder Embeded </a></li>
+                                            <li><a href="list-builder-popup.html">List Builder Popup</a></li>
+                                            <li><a href="list-builder-scroll-box.html">List Builder Scroll Box </a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Component</a>
+                                        <ul>
+                                            <li><a href="accordion-page.html">Accordion</a></li>
+                                            <li><a href="tabs-page.html">Tabs</a></li>
+                                            <li><a href="alert-page.html">Alert</a></li>
+                                            <li><a href="table-page.html">Tables</a></li>
+                                            <li><a href="button-page.html">Buttons</a></li>
+                                            <li><a href="social-media-page.html">Social Media</a></li>
+                                            <li><a href="call-to-action-page.html">Call To Actions</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Gallery</a>
+                                        <ul>
+                                            <li><a href="gallery-popup.html">Gallery Popup</a></li>
+                                            <li><a href="gallery-filterable.html">Gallery Filterable</a></li>
+                                            <li><a href="gallery-carousel.html">Gallery Carousel</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">404 Error</a>
+                                        <ul>
+                                            <li><a href="error-page.html">404 Error 1</a></li>
+                                            <li><a href="error-page-v2.html">404 Error 2</a></li>
+                                            <li><a href="error-page-v3.html">404 Error 3</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="faq-page.html">Faq</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Blog </a>
+                                <ul>
+                                    <li><a href="blog-page.html">Blog Regular</a></li>
+                                    <li><a href="blog-3-column-page.html">Blog Listing 3 Column</a></li>
+                                    <li><a href="blog-single-page.html">Blog Single</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Contactus </a>
+                                <ul>
+                                    <li><a href="contact-page.html">Contact Us</a></li>
+                                    <li><a href="location-page.html">Location</a></li>
+                                    <li><a href="free-assessment-enquiry.html">Enquiry Page</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.header classic -->
+    <div class="slider">
+        <!-- slider -->
+        <div class="slider-carsoule owl-carousel owl-theme">
+            <div class="item">
+                <img src="images/slider-large-1.jpg" alt="Immigration Consulting Responsive Website Template">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-sm-8 col-md-9 col-8">
+                            <div class="slider-captions">
+                                <h1 class="slider-title dotted-line">IELTS / PTE / TOFEL / English Speaking</h1>
+                                <p class="slider-text"> High-stakes English test for study, migration or work.</p>
+                                <a href="#" class="btn btn-default btn-lg">Start Coaching Today</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/slider-large-2.jpg" alt="Immigration visa consultant HTML5 Template">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-sm-8 col-md-9 col-8">
+                            <div class="slider-captions">
+                                <h1 class="slider-title dotted-line">Immigration &amp; Visa Consultants</h1>
+                                <p class="slider-text">Proin venenatis orci felis, tincidunt sagittis mi lacinia vitae. Vestibulum erat nisi, tincidunt vel lobortis eu, imperdiet eu sapien.</p>
+                                <a href="#" class="btn btn-default btn-lg">BOOK A CONSULTATION</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/slider-large-3.jpg" alt="Immigration Consulting Bootstrap Responsive Website Template">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-sm-8 col-md-9 col-8">
+                            <div class="slider-captions">
+                                <h1 class="slider-title dotted-line">World Most Trusted Immigration Company</h1>
+                                <p class="slider-text">Vestibulum erat nisi, tincidunt vel lobortis eu, imperdiet eu sapien. Proin venenatis orci felis, tincidunt sagittis mi lacinia vitae. </p>
+                                <a href="#" class="btn btn-default btn-lg">BOOK A CONSULTATION</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
