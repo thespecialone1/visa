c943eb8c7103939924a6667135670f4e35f1a358
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-10-07 19:44:34 +0500

    /migrate/canada/ page complete

--- migrate/canada/index.php
+++ migrate/canada/index.php
@@ -2,6 +2,13 @@
 require("./../../partials/header.php")
 ?>
 <br><br><br><br><br><br><br>
+<style>
+    .card-img-top {
+        width: 100%;
+        height: 175px;
+        object-fit: cover;
+    }
+</style>
 
 <section class="container">
     <div class="row ">
@@ -24,7 +31,6 @@ require("./../../partials/header.php")
     <div class="row mt-5 justify-content-md-center">
         <div class="col-12 col-md-4">
             <h1>ABOUT CANADA</h1>
-
         </div>
         <div class="col-12 col-md-8">
             <ul class="list-group list-group-flush check-circle listnone" style="margin-bottom: 0; ">
@@ -39,7 +45,9 @@ require("./../../partials/header.php")
                 </p>
             </ul>
         </div>
-        <div class="col-8 my-5">
+        <div class="col-12 col-md-4">
+        </div>
+        <div class="col-12 col-md-8 my-2">
             <table class="table">
                 <thead>
                     <tr>
@@ -71,10 +79,235 @@ require("./../../partials/header.php")
                 </tbody>
             </table>
         </div>
+
+        <div class="col-12 col-md-4">
+            <h1>Salery Scenario in Canada</h1>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>
+                Canada offers best wages for professions that are in high demand in the country. For your consideration, here are some professional areas and the wages offered in Canada in 2022:
+            </p>
+        </div>
+
+        <div class="col-12 col-md-4"></div>
+        <div class="col-12 col-md-8 my-2">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Professional area </th>
+                        <th scope="col">Average Annual Salary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>IT</td>
+                        <td>CAD 69,006</td>
+                    </tr>
+                    <tr>
+                        <td>Engineering</td>
+                        <td>CAD 67,301</td>
+                    </tr>
+                    <tr>
+                        <td>Finance</td>
+                        <td>CAD 67,301</td>
+                    </tr>
+                    <tr>
+                        <td>Human Resource</td>
+                        <td>CAD 58,497</td>
+                    </tr>
+                    <tr>
+                        <td>Hospitality</td>
+                        <td>CAD 43,110</td>
+                    </tr>
+                    <tr>
+                        <td>Healthcare </td>
+                        <td>CAD 51,402</td>
+                    </tr>
+                    <tr>
+                        <td>Marketing</td>
+                        <td>CAD 50,000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+        <div class="col-12 col-md-4">
+            <h1>A future in Canada</h1>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>
+                An LSIC (Longitudinal Survey of Immigrants to Canada) survey was conducted to know whether immigrants would choose to migrate to Canada if they had to make the decision again. Close to 3/4th of the respondents was willing to undertake Canada immigration. Around 84% of participants in the survey were positive about their choice of immigrating to Canada.
+            </p>
+            <P>
+                Here are certain facts that you can learn about life in Canada, which can influence your decision to plan a future in Canada:
+            </P>
+            <ul>
+                <li>More than just the economic outcomes, Canadian immigrants have faith in finding an improvement in their general quality of life in Canada.</li>
+                <li>Groups of immigrants settling in Canada arriving from countries with lower GDP are seen to derive larger differences in life satisfaction. This is higher compared to immigrants arriving in Canada from high GDP nations.</li>
+                <li>Canadian employers are looking to add 49% new permanent job positions in 2022. 45% of total hirings to permanent jobs will be to hire skilled candidates to vacant positions.</li>
+            </ul>
+            <p>Canadian employers are willing to offer 47% of hired workers the option of remote working. 46% of the workers will be up for increased starting salaries. For 31% recruits skills, experience, or education requirements may be relaxed.</p>
+        </div>
+
+    </div>
+
+    <h1 class="text-center mt-5">PROMINENT CITIES IN CANADA</h1>
+    <div class="row justify-content-md-center">
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+            <div class="card">
+                <img class="card-img-top" src="./images/torronto.jpeg" alt="Card image cap">
+                <div class="card-body">
+                    <h2 class="card-text">Toronto</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+            <div class="card">
+                <img class="card-img-top" src="./images/Edmonton-Canada-Alberta.webp" alt="Card image cap">
+                <div class="card-body">
+                    <h2 class="card-text">Edmonton</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+            <div class="card">
+                <img class="card-img-top" src="./images/Montreal.jpeg" alt="Card image cap">
+                <div class="card-body">
+                    <h2 class="card-text">Montreal</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+            <div class="card">
+                <img class="card-img-top" src="./images/Oakville.jpeg" alt="Card image cap">
+                <div class="card-body">
+                    <h2 class="card-text">Oakville</h2>
+                </div>
+            </div>
+        </div>
     </div>
 
 
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <h2>WHY IMMIGRANTS CHOOSE TO MOVE TO CANADA?</h2>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>Every year thousands of expats migrate to Canada from UAE. Even though UAE boasts a high quality of life, many of them choose Canada immigration because of these interesting reasons:</p>
+            <ul>
+                <li>An easy path to citizenship</li>
+                <li>Universal health care facilities</li>
+                <li>A plethora of nature and wildlife</li>
+                <li>Flourishing career opportunities</li>
+                <li>High-quality standards in education</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <h1>A HISTORY OF BUILDING A FUTURE IN CANADA</h1>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>Canada is popularly known as the friendliest immigration nation in the world. “The Great White North” as it’s called, has amazing immigration plans for the period ending in 2024. Not just that! The IRCC has announced an investment of $100 million for the settlement of newcomers.</p>
+            <p>On December 23, 2021, Canada proudly announced the country’s historical moment, as they landed 405,000 immigrants in 2021. The country has activated various immigration streams in order to attract foreign nationals. There are more than 100 immigration pathways that provide every expat the opportunity to migrate to the country.</p>
+            <p>These incredible steps to welcome expats make Canada the best immigrant-friendly country for immigrants from the UAE.</p>
+        </div>
+    </div>
 </section>
+
+
+<section class="container">
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <h2>CANADA IMMIGRATION ELIGIBILITY</h2>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <h2>The eligibility criteria for Canada Immigration differ for each program like:</h2>
+            <ul>
+                <li>Express Entry Program</li>
+                <li>Quebec Skilled Worker Program (QSWP)</li>
+                <li>Provincial Nominee Program (PNP)</li>
+            </ul>
+            <img src="./images/REQUIREMENTS-FOR-CANADA-IMMIGRATION.jpg" class="img-fluid" alt="Responsive image">
+            <p>Below is the list of general eligibility requirements, which is common for all. These include:</p>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">General eligibility </th>
+                        <th scope="col">Specificities</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Age</td>
+                        <td>18 years and above</td>
+                    </tr>
+                    <tr>
+                        <td>Points</td>
+                        <td>67</td>
+                    </tr>
+                    <tr>
+                        <td>Educational qualifications</td>
+                        <td>Educational qualifications equal to higher secondary education in Canada</td>
+                    </tr>
+                    <tr>
+                        <td>Language proficiency tests</td>
+                        <td>IELTS (6+) or CLB (4+)</td>
+                    </tr>
+                    <tr>
+                        <td>Work Experience</td>
+                        <td>Minimum of one year’s work experience</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <h3>HOW TO APPLY FOR CANADA IMMIGRATION ONLINE APPLICATION?</h3>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>Canada Immigration from the UAE is easy, as it has a very clear process for each pathway of immigration.</p>
+            <img src="./images/Canada-Immigration-from-the-UAE.jpg" class="img-fluid" alt="Responsive image">
+            <p>You may follow the steps below to make the process simple.</p>
+            <h2>Step 1: Eligibility Check</h2>
+            <p>It’s the prime step for Canada immigration!</p>
+            <p>Checking your eligibility for Canada immigration is the foremost step. Use the Y-Axis Canada Points calculator for free to check your score and eligibility.</p>
+            <p>Note: You need to score a minimum of 67 points to get eligible for Canada Immigration, and to boost your chances for Canada PR Visa you need to score more.</p>
+            <h2>Step 2: English proficiency Test</h2>
+            <p>Test your language proficiency in CBL or IELTS or TOEFL.
+                <br> Prove your language proficiency in CBL or IELTS, or TOEFL by scoring high points. A high score in language proficiency tests will certainly boost your chances for Canada immigration.
+                <br> Note: Get a positive impact on your score through Y-Axis Coaching for language proficiency, which provides world-class coaching that helps you to score more in the language proficiency test.
+            </p>
+
+            <h2>Step 3: Skills Assessment</h2>
+            <P>Assess your skillset for Canada Immigration! <br><br>
+                Assessing your skillset can be done through the relevant skill assessment body of the country.</P>
+
+            <h2>Step 4: Expression of Interest (EOI)</h2>
+            <P>Be cautious while registering your EOI for Canada Immigration! <br><br>
+                You have to be very attentive while registering your Expression of Interest (EOI) for the Canada Express Entry portal, as it plays a crucial role in your Canada work visa or Canada PR visa.</P>
+
+            <h2>Step 5: Invitation to Apply (ITA)</h2>
+            <P>Obtaining ITA, essential for Canada immigration! <br><br>
+                If you receive an Invitation to Apply (ITA) from any of the Canadian immigration pathways, you will be given 60 calendar days to apply for any of the specific Canada immigration pathways such as Canada Express Entry, Provincial Nominee Pathways and Quebec Immigration programs.</P>
+
+            <h2>Step 6: Apply for Canada PR Visa</h2>
+            <P>Embrace your chances for permanent residency with a Canada PR Visa! <br><br>
+                You can apply for permanent residency online through Canada PR visa by following the accurate procedures and submitting the requirements.</P>
+
+        </div>
+    </div>
+
+</section>
+<section class="mt-10"> &nbsp;</section>
 
 <?php
 require("./../../partials/footer.php")
