05960121434f7c122b8832dce8b429c1847e9a71
thespecialone1 <76862693+thespecialone1@users.noreply.github.com>
2022-10-07 04:50:00 -0700

    header fix, index visa added etc

---
+++ visa/study/index.php
@@ -0,0 +1,14 @@
+<?php
+require("./../../partials/header.php");
+?>
+<div class="container">
+    <br><br><br><br><br><br><br>
+    <h1>mazan labeeb</h1>
+</div>
+
+
+
+
+<?php
+require("./../../partials/footer.php");
+?>
