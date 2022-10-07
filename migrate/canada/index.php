c02312c923a86e73f71ef0ef3a335d5a0c58451d
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-10-07 18:54:57 +0500

    Update

--- migrate/canada/index.php
+++ migrate/canada/index.php
@@ -1,8 +1,81 @@
 <?php
-    require("./../../partials/header.php")
+require("./../../partials/header.php")
 ?>
+<br><br><br><br><br><br><br>
 
+<section class="container">
+    <div class="row ">
+        <div class="col-12 col-md-4">
+            <h1>Why Migrate to Canada?</h1>
+
+        </div>
+        <div class="col-12 col-md-8">
+            <ul class="list-group list-group-flush check-circle listnone" style="margin-bottom: 0; ">
+                <li class="list-group-item" style="padding:0 1.5rem">Canada to add 1.3 million immigrants by 2024</li>
+                <li class="list-group-item" style="padding:0 1.5rem">High minimum wage</li>
+                <li class="list-group-item" style="padding:0 1.5rem">High quality of life including free basic healthcare</li>
+                <li class="list-group-item" style="padding:0 1.5rem"> Talents in demand in tech, healthcare, and retail industries</li>
+                <li class="list-group-item" style="padding:0 1.5rem">Numerous pathways to permanent residency</li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="row mt-5 justify-content-md-center">
+        <div class="col-12 col-md-4">
+            <h1>ABOUT CANADA</h1>
+
+        </div>
+        <div class="col-12 col-md-8">
+            <ul class="list-group list-group-flush check-circle listnone" style="margin-bottom: 0; ">
+                <p>
+                    An economy to look forward to…
+                </p>
+                <p>
+                    Canada has one of the world’s most progressive economies that depends a lot on foreign immigrants to contribute to the economic development of the country. In the first quarter of 2022, the country’s GDP grew by 0.8%.
+                </p>
+                <p>
+                    The main strength of the Canadian economy is derived from increased investment in business as well as increase in household consumption. Here’s a comparison of GDP growth in 2022 between Canada and other prominent countries in Q1 of 2022.
+                </p>
+            </ul>
+        </div>
+        <div class="col-8 my-5">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Country </th>
+                        <th scope="col">GDP growth (Q1 2022)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Canada</td>
+                        <td>0.8</td>
+                    </tr>
+                    <tr>
+                        <td>USA</td>
+                        <td>-0.4</td>
+                    </tr>
+                    <tr>
+                        <td>Germany</td>
+                        <td>0.2</td>
+                    </tr>
+                    <tr>
+                        <td>UK</td>
+                        <td>0.8</td>
+                    </tr>
+                    <tr>
+                        <td>Australia</td>
+                        <td>0.8</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+</section>
 
 <?php
-    require("./../../partials/footer.php")
+require("./../../partials/footer.php")
 ?>
