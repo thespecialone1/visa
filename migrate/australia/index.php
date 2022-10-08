d372480206c027cdccb12be69cddbb076c818fd4
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-10-08 19:16:22 +0500

    migrate/australia added

---
+++ migrate/australia/index.php
@@ -0,0 +1,222 @@
+<?php
+require("./../../partials/header.php")
+?>
+<style>
+    .card-img-top {
+        width: 100%;
+        height: 175px;
+        object-fit: cover;
+    }
+
+    .page-header {
+        background: url(images/australia.jpg) no-repeat center !important;
+    }
+</style>
+
+
+<div class="page-header ">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                <h2 class="page-title   text-shadow-black">Migrate to Australia</h2>
+                <p class="page-description  text-shadow-black font-weight-light">
+                    As per the UN report on migration, over 280 million migrants are currently on the globe. They are finding better places to live happier lives, have rewarding careers, and enjoy a more inclusive social environment.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="container mt-5">
+    <div class="row ">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Why Migrate to Australia?</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8">
+            <ul class="list-group list-group-flush check-circle listnone" style="margin-bottom: 0; ">
+                <li class="list-group-item" style="padding:0 1.5rem">Welcoming at least 160,000 immigrants in 2022-23</li>
+                <li class="list-group-item" style="padding:0 1.5rem"> High minimum wage (AUD 40,200 approx.)</li>
+                <li class="list-group-item" style="padding:0 1.5rem">Dual citizenship</li>
+                <li class="list-group-item" style="padding:0 1.5rem"> One of the strongest economies in the world</li>
+                <li class="list-group-item" style="padding:0 1.5rem">High-quality healthcare</li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="row mt-5 justify-content-md-center">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Why Immigrants Choose Australia</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8">
+            <ul class="list-group list-group-flush check-circle listnone" style="margin-bottom: 0; ">
+                <p>
+                    Australia is popularly known as the land of opportunities as it welcomes lakhs of immigrants every year to live, work and study in the country. Australia is the perfect place for immigrants from the UAE to settle because of its ideal attractions and benefits like
+                </p>
+                <img src="./images/img-1.jpg" class="img-fluid mb-5" alt="Responsive image">
+                <ul class="list-group list-group-flush check-circle listnone" style="margin-bottom: 0; ">
+                    <li class="list-group-item" style="padding:0 1.5rem">Well-defined living rights</li>
+                    <li class="list-group-item" style="padding:0 1.5rem"> Growing employment opportunities</li>
+                    <li class="list-group-item" style="padding:0 1.5rem">Social security</li>
+                    <li class="list-group-item" style="padding:0 1.5rem"> High standards of living</li>
+                    <li class="list-group-item" style="padding:0 1.5rem">World-class educational facilities for children</li>
+                </ul>
+                <p>Australia immigration offers a perfectly organized immigration system and visa options for immigrants from UAE.</p>
+            </ul>
+        </div>
+
+        <div class="col-12 col-md-4 mt-5">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>How to process with Australia Immigration?</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 my-2 mt-5">
+            <p>
+                The world’s most urbanized country, Australia, welcomes many immigrants worldwide as it has a wide range of job openings in fields like Engineering, Healthcare, IT, Construction and Mining, Manufacturing, Tourism, and Accounts and Finance.
+            </p>
+        </div>
+
+
+        <div class="col-12 col-md-4 mt-5">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>How can you migrate to Australia?</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 my-2 mt-5">
+            <p>
+                There are various paths for Australian immigration from UAE.
+            </p>
+            <div class="card border-dark mb-3">
+                <div class="card-header bg-secondary">Major streams through which you can immigrate to Australia</div>
+                <div class="card-body text-dark" style="padding: 0.2rem 1rem !important;padding-bottom: 0px !important;">
+                    <ul class="list-group list-group-flush check-circle listnone">
+                        <li class="list-group-item" style="padding:0 1.5rem">Well-defined living rights</li>
+                        <li class="list-group-item" style="padding:0 1.5rem"> Growing employment opportunities</li>
+                        <li class="list-group-item" style="padding:0 1.5rem">Social security</li>
+                        <li class="list-group-item" style="padding:0 1.5rem"> High standards of living</li>
+                        <li class="list-group-item" style="padding:0 1.5rem">World-class educational facilities for children</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Visa Categories for Australia Immigration</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>The visa categories for Australia immigration are given below:</p>
+            <table class="table">
+                <thead class="table-secondary">
+                    <tr>
+                        <th scope="col">Sub Class </th>
+                        <th scope="col">Type of visa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Subclass 189</td>
+                        <td>Independent Skilled Visa</td>
+                    </tr>
+                    <tr>
+                        <td>Subclass 190</td>
+                        <td>State Nomination</td>
+                    </tr>
+                    <tr>
+                        <td>Subclass 491</td>
+                        <td>Regional Sponsorship</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Eligibility criteria for Australia Immigration</h1>
+            </div>
+
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>Generally, for Australia immigration, the major requirement is a minimum score of 65 points for an Australia work visa. However, if your score is between 80-85 points there are more chances for Australia immigration with an Australia PR visa. The score is calculated based on age, education, qualification, work experience, adaptability, etc. </p>
+        </div>
+    </div>
+</section>
+
+
+<section class="container">
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Australia Immigration Requirements</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <ul>
+                <li>A minimum score of 65 in the Points Gri</li>
+                <li>Aged below 45 years</li>
+                <li>Skill assessment by certified authorities in Australi</li>
+                <li>PTE Score 50 or IELTS score 6 (in each component)</li>
+                <li>An occupation that’s listed in Australia’s SOL (Skilled Occupation List) or PMSOL (Priority Migration Skilled Occupation List)</li>
+            </ul>
+
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>How to apply for Australia Immigration online from UAE?</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>Applying to Australia Immigration from UAE is simple, as the country has a clear well-framed procedure. Below is the step-by-step procedure to easily apply for the process.</p>
+            <img src="./images/Step-by-Step-Guide-to-Australia-PR-Visa.jpg" class="img-fluid mb-5" alt="Responsive image">
+
+            <p>You may follow the steps below to make the process simple.</p>
+            <h2 class="text-primary font-weight-light">Step 1: Eligibility Check</h2>
+            <p>It’s the prime step for Australia immigration!</p>
+            <p>Checking your eligibility for Australia immigration is the foremost step.</p>
+           <p>Note: You must score at least 65 points to become eligible for Australia skilled migration. To boost your chances for Australia PR Visa, you need a score between 80-85. </p>
+            <h2 class="text-primary font-weight-light">Step 2: English proficiency Test</h2>
+            <p>Test your language proficiency in CBL or IELTS or TOEFL.
+                <br> Prove your language proficiency in CBL or IELTS, or TOEFL by scoring high points. A high score in language proficiency tests will certainly boost your chances for Australia immigration.
+            </p>
+
+            <h2 class="text-primary font-weight-light">Step 3: Skills Assessment</h2>
+            <P>Assess your skillset for Australia Immigration! <br><br>
+                Assessing your skillset can be done through the relevant skill assessment body of the country.</P>
+
+            <h2 class="text-primary font-weight-light">Step 4: Expression of Interest (EOI)</h2>
+            <P>Be cautious while registering your EOI for Australia Immigration! <br><br>
+                You have to be very attentive while registering your Expression of Interest (EOI) for the Australia Express Entry portal, as it plays a crucial role in your Australia work visa or Australia PR visa.</P>
+
+            <h2 class="text-primary font-weight-light">Step 5: Invitation to Apply (ITA)</h2>
+            <P>Obtaining ITA, essential for Australia immigration! <br><br>
+                If you receive an Invitation to Apply (ITA) from any of the Canadian immigration pathways, you will be given 60 calendar days to apply for any of the specific Australia immigration pathways such as Australia Express Entry, Provincial Nominee Pathways and Quebec Immigration programs.</P>
+
+            <h2 class="text-primary font-weight-light">Step 6: Apply for Australia PR Visa</h2>
+            <P>Embrace your chances for permanent residency with a Australia PR Visa! <br><br>
+                You can apply for permanent residency online through Australia PR visa by following the accurate procedures and submitting the requirements.</P>
+
+        </div>
+    </div>
+
+</section>
+<section class="mt-10"> &nbsp;</section>
+
+<?php
+require("./../../partials/footer.php")
+?>
