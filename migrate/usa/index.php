d9b6b49585c0a73aadc78529f7d7b4e24a5fa8ba
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-10-09 19:32:45 +0500

    migrate section completed

---
+++ migrate/usa/index.php
@@ -0,0 +1,141 @@
+<?php
+require("./../../partials/header.php")
+?>
+<style>
+    .card-img-top {
+        width: 100%;
+        height: 175px;
+        object-fit: cover;
+    }
+
+    .page-header {
+        background: url(images/usa.jpg) no-repeat center !important;
+    }
+</style>
+
+
+<div class="page-header ">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                <h2 class="page-title   text-shadow-black">Migrate to USA</h2>
+                <p class="page-description  text-shadow-black font-weight-light">
+                    As per the UN report on migration, over 280 million migrants are currently on the globe. They are finding better places to live happier lives, have rewarding careers, and enjoy a more inclusive social environment.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="container mt-5">
+    <div class="row ">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Why Migrate to USA?</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8">
+            <ul class="list-group list-group-flush check-circle listnone" style="margin-bottom: 0; ">
+                <li class="list-group-item" style="padding:0 1.5rem">428,000 jobs in 20 sectors, 2022</li>
+                <li class="list-group-item" style="padding:0 1.5rem"> Top QS ranked educational institutions</li>
+                <li class="list-group-item" style="padding:0 1.5rem"> Build a career at the world’s best companies</li>
+                <li class="list-group-item" style="padding:0 1.5rem"> Annual wages starting from $15,072</li>
+                <li class="list-group-item" style="padding:0 1.5rem">High standard of living</li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="row mt-5 justify-content-md-center">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Why Immigrants Choose USA</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8">
+            <p>Thousands of expats choose the US as their dream destination to immigrate and settle here every year.</p>
+            <p>The country has left lasting imprints on global culture. The USA has great resources, and the massive land area provides lots of opportunities for skilled professionals. The country’s stable economy, top-class education system, and many other factors symbolize the country for personal or professional growth.</p>
+            <p class="text-danegr">The average annual salary in the US offered is ten times more than the annual salary in several countries in the Middle East.</p>
+        </div>
+
+        <div class="col-12 col-md-4 ">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Opportunities in USA</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 my-2 ">
+            <p>USA Immigration offers the following to immigrants which makes it so attractive a choice to the USA:</p>
+            <ul class="list-group list-group-flush check-circle listnone">
+                <li class="list-group-item" style="padding:0 1.5rem">Educational opportunities</li>
+                <li class="list-group-item" style="padding:0 1.5rem">Flourishing job opportunities</li>
+                <li class="list-group-item" style="padding:0 1.5rem">Well-recognized university</li>
+                <li class="list-group-item" style="padding:0 1.5rem"> High standard of living</li>
+                <li class="list-group-item" style="padding:0 1.5rem">Easy to obtain Citizenship</li>
+                <li class="list-group-item" style="padding:0 1.5rem">Financial security for the entire family</li>
+            </ul>
+            <p>According to NFAP analysis, the country has more than 1.2 million job vacancies and welcomes foreign nationals to fill these vacancies through USA Immigration. Hence most immigrants migrate through the USA work visa.</p>
+        </div>
+
+
+        <div class="col-12 col-md-4 mt-5">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>How can you migrate to USA?</h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-8 my-2 mt-5">
+            <p>There are several ways for USA immigration. The most common ones include:</p>
+            <h3 class="text-danger font-weight-light">Employment-based immigration (USA work visa)</h3>
+            <p>People looking for USA immigration can migrate by signing a contract with a company in the US. For this employment-based immigration, you need to search for the relevant opportunity in the country. Here work experience and educational qualifications play a crucial role. But the visa issued under this type of USA immigration through USA work visa offers a great opportunity to establish a livelihood in the country.</p>
+            <h3 class="text-danger font-weight-light">Green Card Lottery</h3>
+            <p>The Green Card Lottery program is popularly known as the Diversity Immigrant Visa Program. This program annually issues 50,000 permanent residence permits (Green Cards). Candidates who meet the requirements can easily get through the Green Card Lottery.</p>
+            <h3 class="text-danger font-weight-light">Family-based immigration</h3>
+            <p>If you have a US citizen or Green Card holder family member, then you can choose Family-based immigration. Based on the degree of kinship, it is divided into two categories.</p>
+            <p>The first category includes fiancé, spouse, unmarried children under the age of 21, and parents of US citizens or Green Card holders. They will receive their permanent residence permit within time.</p>
+            <p>The second is for unmarried children above 21 years of age, of US citizens or Green Card holders. For them, it will take longer to get permanent residency.</p>
+
+            <h3 class="text-danger font-weight-light">Immigration via investment</h3>
+            <p>
+                USA immigration through investment would be interesting for self-employed individuals. If you can, plan to create equity to provide jobs for at least ten people. But the capital required varies between $900,000 and $1,800,000 depending on the state.
+            </p>
+            <h3 class="text-danger font-weight-light">USA Study Visa</h3>
+            <p>You can even migrate to the USA through USA study visa which is simple and easy to obtain. Pursuing your education in a country like the USA and getting employed there henceforth will boost your chances of settling abroad as a permanent resident.</p>
+
+
+            <h3 class="text-danger font-weight-light">USA Visit Visa</h3>
+            <p>Migrating through a USA visit visa is also possible. However, you cannot stay in the country for a long duration.</p>
+
+
+        </div>
+
+    </div>
+
+
+
+
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <div class="heading-border-style textleft-onlgscreen-textcenter-onsmscreen">
+                <h1>Eligibility Requirements for USA Immigration</h1>
+            </div>
+
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <p>The eligibility criteria for USA Immigration from UAE differs for each program (USA work visa, investment visa, or dependent visa). Below is the list of general eligibility requirements, which is common for all. These include:</p>
+            <ul>
+                <li><b>Age:</b> You should be 18 years of age.</li>
+                <li><b>Educational qualifications:</b> You should have the minimum educational qualifications equal to higher secondary education in the USA.</li>
+                <li><b>Language proficiency tests:</b> You should score minimum marks in language proficiency tests such as IELTS (6+) or TOEFL.</li>
+                <li><b>Work Experience:</b> You should have at least one year’s work experience in the field in which you are applying for.</li>
+                <li><b>Job offer in the USA:</b> You can migrate to the USA with or without a job offer.</li>
+            </ul>
+        </div>
+    </div>
+
+
+
+</section>
+<section class="mt-10"> &nbsp;</section>
+
+<?php
+require("./../../partials/footer.php")
+?>
