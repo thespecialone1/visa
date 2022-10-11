c6adb2e8482ad48d35f198be744357ce46c1b0c8
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-10-11 23:55:48 +0500

    accordion added

--- visa/business/canada/index.php
+++ visa/business/canada/index.php
@@ -167,6 +167,98 @@ require("./../../../partials/header.php")
 
 
 </section>
+
+<section class="container faq">
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+        </div>
+        <div class="col-12 col-md-8 my-2">
+            <div id="accordion">
+                <div class="card-accordion">
+                    <div class="card-accordion-header" id="headingOne">
+                        <h5 class="mb-0">
+                            <button class="accordion-btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                How long can I stay in Canada on a Business Permit?
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion" style="">
+                        <div class="card-accordion-body">
+                            <p>The Canada Business Permit is a provisional. It has a maximum validity of 6 months. It’s advisable that you stay for the period that you have specified in the application, even if it’s less than 6 months. The Embassy can offer you a permit that has longer validity such as 3, 5, or 10 years. However, you must not remain for more than 6 months at a time.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-accordion">
+                    <div class="card-accordion-header" id="headingTwo">
+                        <h5 class="mb0">
+                            <button class="collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                How do I get a Canadian Business Permit?
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion" style="">
+                        <div class="card-accordion-body">
+                            <p>The 5 stages given below are common in any application for Canada Business Permit irrespective of the business immigration stream you choose to apply:</p>
+                            <h2 class="text-primary">Check your eligibility</h2>
+                            <p>
+
+                                The first step is to assess if you fulfil the requires of the business program that you are interested in.
+                            </p>
+                            <h2 class="text-primary">
+                                Select the program that best suits your business
+                            </h2>
+                            <p>
+
+                                You must select the category that’s most suitable for the business type that you want to expand overseas in Canada.
+                            </p>
+                            <h2 class="text-primary">
+                                Collate the required paperwork
+                            </h2>
+                            <p>
+                                This is undoubtedly the most tiresome part of the process of business immigration to Canada. You must have all the records of information that will be a poof of fulfilling the requirements of the chosen business stream.
+
+                            </p>
+                            <h2 class="text-primary">
+                                Confirm submission format and deadlines
+                            </h2>
+
+                            <p>
+                                There can be some tough deadlines to be met depending on your chosen business immigration stream. This is to ensure that your application remains valid.
+
+                            </p>
+                            <h2 class="text-primary">
+                                Obtain a Permit or an ETA
+                            </h2>
+
+                            <p>
+
+                                You will have to get ready for your trip to Canada once your application is successful. You can obtain the Permit or the ETA (Electronic Travel Authorization).
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="card-accordion">
+                    <div class="card-accordion-header" id="headingThree">
+                        <h5 class="mb-0">
+                            <button class="collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                How long does it take to get a Canada Business Permit?
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion" style="">
+                        <div class="card-accordion-body">
+                            <p>This will depend on the workload of the Embassy of Canada that you have applied to. You may have to wait for 1 to 6 weeks for processing your Canada Business Permit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</section>
+
 <section class="mt-10"> &nbsp;</section>
 
 <?php
